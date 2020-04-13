1f7bfad35d5b81afaf29fb82bf0a2d52088f367c
chunpingyang <cxy180010@utdallas.edu>
2020-04-13 17:48:44 -0500

    新增 SharedDataAmongCohortCoordThreads.java

---
+++ src/utility/SharedDataAmongCohortCoordThreads.java
@@ -0,0 +1,35 @@
+package utility;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SharedDataAmongCohortCoordThreads {
+
+    /**
+     * A class that contains various updated values that can be used by one server and client threads
+     *
+     */
+    private Map<String, Map<String,Boolean>> agreeMap;
+
+    public SharedDataAmongCohortCoordThreads(int maxCoordinator){
+        String[] fileList = new String[]{"1","2","3","4"};
+        agreeMap = Collections.synchronizedMap(new HashMap<>());
+        for(int i=0;i<maxCoordinator;i++){
+            Map<String,Boolean> fileMap = Collections.synchronizedMap(new HashMap<>());
+            for(String fileId:fileList){
+                fileMap.put(fileId,false);
+            }
+            String clientId = String.valueOf((i+1));
+            agreeMap.put(clientId,fileMap);
+        }
+    }
+
+    public Map<String, Map<String, Boolean>> getAgreeMap() {
+        return agreeMap;
+    }
+
+    public boolean isAgree(String clientId,String fileId){
+        return agreeMap.get(clientId).get(fileId);
+    }
+}
